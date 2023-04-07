96df56140f90de6236b116aafde8b8ef134d9775
devteam <77335157+e2msolutions-it@users.noreply.github.com>
2023-04-07 11:36:47 +0530

    changes done

--- wp-content/themes/genesis-block-theme/template-parts/content.php
+++ wp-content/themes/genesis-block-theme/template-parts/content.php
@@ -12,11 +12,11 @@
 		<header class="entry-header">
 			<?php if ( is_single() ) { ?>
 				<h1 class="entry-title">
-					yagnik<?php// the_title(); ?>
+					<?php// the_title(); ?>
 				</h1>
 			<?php } else { ?>
 				<h2 class="entry-title">
-					<a href="<?php the_permalink(); ?>" rel="bookmark">Yagnik<?php //the_title(); ?></a>
+					<a href="<?php the_permalink(); ?>" rel="bookmark">Yagnik</a>
 				</h2>
 			<?php } ?>
 
